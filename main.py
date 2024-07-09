22f6fed622ab2fd61c4345685d4d4a4832f6e322
OscarNavaJ <onavaj@gmail.com>
2024-07-09 15:47:08 -0600

    Usb connection methods

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from middleware.modules.no_usb_metadata import NoUsbDevice
 
 if __name__ == '__main__':
     ometeo_no_usb = NoUsbDevice()
-    ometeo_no_usb.vcc_on()
+    #ometeo_no_usb.micro_reset()
+    #sleep(5)
+    ometeo_no_usb.get_micro_id()
+    ometeo_no_usb.usb_disconnect()
     sleep(5)
-    ometeo_no_usb.vcc_off()
+    ometeo_no_usb.usb_connect()
--- middleware/modules/no_usb_metadata.py
+++ middleware/modules/no_usb_metadata.py
@@ -25,12 +25,12 @@ class NoUsbDetectHidDevice:
             print(f'An Exception Occurred: {ex}')
             print('Please verify your NO USB device connection !')
 
-        _vendor_id_str = str(device_meta['vendor_id'])
+        _vendor_id_str = str(device_meta['product_id'])
         with open(DEVICE_DATA_JSON, 'r+') as json_file:
             json_data = json.load(json_file)
             if _vendor_id_str in json_data:
                 device_meta.update(json_data[_vendor_id_str])
-                
+                #'800006940800'
         return device_meta
         
 
@@ -38,7 +38,12 @@ class NoUsbDevice(NoUsbDetectHidDevice):
     def __init__(self) -> None:
         super().__init__()
         self.major_cmd = self.device_meta['major_command']
-        
+        self.info_cmd = 1
+
+    def get_micro_id(self, port=None):
+        if not self.device_meta['multi_port']:
+            port = 1
+        self._report_transaction((self.info_cmd,))
     def vcc_on(self, port=None):
         if not self.device_meta['multi_port']:
             port = 1
@@ -50,7 +55,18 @@ class NoUsbDevice(NoUsbDetectHidDevice):
             port = 1
             
         self._report_transaction((self.major_cmd, port, 0))
-    
+
+    def usb_connect(self, port=None):
+        if not self.device_meta['multi_port']:
+            port = 1
+        self._report_transaction((self.major_cmd, port, 9))
+    def usb_disconnect(self, port=None):
+        if not self.device_meta['multi_port']:
+            port = 1
+        self._report_transaction((self.major_cmd, port, 10))
+
+    def micro_reset(self):
+        self._report_transaction((self.major_cmd, 1, 99))
     def _report_transaction(self, write_data : tuple):
         self.device.write([1,*write_data] + [0] * 60)
         sleep(0.05)
