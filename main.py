11b105afaa7c03e8c00e98620d11b1a59e189613
OscarNavaJ <onavaj@gmail.com>
2024-08-06 11:30:11 -0600

    NOUSB Refactor

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from time import sleep
-from middleware.modules.no_usb_metadata import NoUsbDevice
-
+from middleware.modules.no_usb_metadata import get_NoUsbDevice
+from middleware.modules.no_usb_metadata import NoUSBDeviceFoundError
 if __name__ == '__main__':
-    ometeo_no_usb = NoUsbDevice()
-    #ometeo_no_usb.micro_reset()
-    #sleep(5)
-    ometeo_no_usb.get_micro_id()
-    ometeo_no_usb.usb_disconnect()
-    sleep(5)
-    ometeo_no_usb.usb_connect()
+    #ometeo_no_usb = NoUsbDevice()
+    try:
+        mk3_no_usb = get_NoUsbDevice()
+        #ometeo_no_usb.micro_reset()
+        #sleep(5)
+        mk3_no_usb.get_micro_id()
+        #ometeo_no_usb.usb_disconnect()
+        sleep(1)
+        mk3_no_usb.upstream_on()
+        sleep(5)
+        mk3_no_usb.pc_on()
+        sleep(5)
+        #mk3_no_usb.device_off()
+        print(mk3_no_usb.connection_status())
+    except NoUSBDeviceFoundError as err:
+        print(err.message)
--- middleware/modules/no_usb_metadata.py
+++ middleware/modules/no_usb_metadata.py
@@ -1,19 +1,55 @@
 import hid
 import json
 from time import sleep
-
 from middleware.settings.config import DEVICE_DATA_JSON
+from abc import ABC, abstractmethod
+from typing import List
 
-class NoUsbDetectHidDevice:
+class NoUSBDeviceFoundError(Exception):
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return self.message
+class NoUsbDetectHidDevice(ABC):
     def __init__(self) -> None:
         self.device = None
         self.device_meta = self._find_device()
-    
-    def _find_device(self) -> dict:
+        self.device_type = self.device_meta['product_string']
+
+    def device_filter(self,device, vendor_id = 'NO USB', product_id = None, serial_pn = None):
+        if not device or device['manufacturer_string'] != vendor_id:
+            return False
+        if product_id and product_id != device['product_string']:
+            return False
+        if serial_pn and product_id != self.get_micro_id():
+            return False
+        return True
+
+    def get_micro_id(self, port=None):
+        if not self.device_meta['multi_port']:
+            port = 1
+        response = self._report_transaction((self.info_cmd,))
+        return response
+
+    def _report_transaction(self, write_data : tuple):
+        self.device.write([1,*write_data] + [0] * 60)
+        sleep(0.05)
+        response = None
+        while True:
+            response = self.device.read(64)
+            if response:
+                print(response)
+                break
+            else:
+                break
+        return response
+
+    def _find_device(self, product_id = None, serial_pn = None) -> dict:
         device_meta = None
-        for hid_device in hid.enumerate():
-            if hid_device['manufacturer_string'] == 'NO USB' and hid_device['product_string'] == 'NO USB':
-                device_meta = hid_device
+        for hid_device in list(filter(lambda x:self.device_filter(x,product_id=product_id,serial_pn=serial_pn),hid.enumerate())):
+            # if hid_device['manufacturer_string'] == 'NO USB' and hid_device['product_string'] == 'NO USB':
+            device_meta = hid_device
+
                 
         try:
             self.device = hid.device()
@@ -21,10 +57,9 @@ class NoUsbDetectHidDevice:
             self.device.set_nonblocking(0)
             print('Device found and running !\n')
             print(f"Device Information-> \nName: {device_meta['product_string']} \nProduct ID: {device_meta['product_id']} \nVendor ID: {device_meta['vendor_id']}")
-        except IOError as ex:
-            print(f'An Exception Occurred: {ex}')
+        except (IOError, TypeError) as ex:
             print('Please verify your NO USB device connection !')
-
+            raise NoUSBDeviceFoundError("No² USB Found")
         _vendor_id_str = str(device_meta['product_id'])
         with open(DEVICE_DATA_JSON, 'r+') as json_file:
             json_data = json.load(json_file)
@@ -34,17 +69,13 @@ class NoUsbDetectHidDevice:
         return device_meta
         
 
-class NoUsbDevice(NoUsbDetectHidDevice):
-    def __init__(self) -> None:
-        super().__init__()
+class OmeteoNoUsbDevice(NoUsbDetectHidDevice):
+    def __init__(self,hid_device) -> None:
+        #super().__init__()
+        self.device = hid_device.device
+        self.device_meta = hid_device.device_meta
         self.major_cmd = self.device_meta['major_command']
         self.info_cmd = 1
-
-    def get_micro_id(self, port=None):
-        if not self.device_meta['multi_port']:
-            port = 1
-        response = self._report_transaction((self.info_cmd,))
-        return response
     def vcc_on(self, port=None):
         if not self.device_meta['multi_port']:
             port = 1
@@ -78,15 +109,48 @@ class NoUsbDevice(NoUsbDetectHidDevice):
     def micro_reset(self):
         response = self._report_transaction((self.major_cmd, 1, 99))
         return response
-    def _report_transaction(self, write_data : tuple):
-        self.device.write([1,*write_data] + [0] * 60)
-        sleep(0.05)
-        response = None
-        while True:
-            response = self.device.read(64)
-            if response:
-                print(response)
-                break
-            else:
-                break
+
+class Mk3NoUsbDevice(NoUsbDetectHidDevice):
+    def __init__(self,hid_device) -> None:
+        #super().__init__()
+        self.device = hid_device.device
+        self.device_meta = hid_device.device_meta
+        self.major_cmd = self.device_meta['major_command']
+        self.info_cmd = 1
+
+    def upstream_on(self, port=None):
+        if not self.device_meta['multi_port']:
+            port = 1
+        response = self._report_transaction((self.major_cmd, port, 8))
         return response
+
+    def device_off(self, port=None):
+        if not self.device_meta['multi_port']:
+            port = 1
+        response = self._report_transaction((self.major_cmd, port, 9))
+        return response
+
+    def pc_on(self, port=None):
+        if not self.device_meta['multi_port']:
+            port = 1
+        response = self._report_transaction((self.major_cmd, port, 10))
+        return response
+
+    def connection_status(self, port=None):
+        if not self.device_meta['multi_port']:
+            port = 1
+        response = self._report_transaction((self.major_cmd, port, 11))
+        return response
+
+    def micro_reset(self):
+        response = self._report_transaction((self.major_cmd, 1, 99))
+        return response
+
+def get_NoUsbDevice( product_id = None, serial_pn = None):
+    hid_device = NoUsbDetectHidDevice()
+    nousb = None
+    if hid_device.device_type in ("Not a double host switch","NO USB", "Mk3"):
+        nousb = Mk3NoUsbDevice(hid_device)
+    elif hid_device.device_type in ("NO USB", "Ometeo", "Mk2"):
+        nousb = OmeteoNoUsbDevice(hid_device)
+    return nousb
